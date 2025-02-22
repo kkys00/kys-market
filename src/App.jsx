d9d83bb156b31d21dda9cb1d3cb3eaf8def37605
Yeongseo Kim <1903020@ewhain.net>
2025-02-22 17:58:32 +0900

    style: App.css에 content 너비 여백 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,26 @@ import Detail from './pages/Detail'
 import New from './pages/New'
 import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
+import Market from './pages/Market'
+import Header from './components/Header'
+import Nav from './components/Nav'
+
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<New />} />
-        <Route path='/detail/:id' element={<Detail />} />
-        <Route path='/edit/:id' element={<Edit />} />
-        <Route path='/*' element={<Notfound />} />
-      </Routes>
+      <Header />
+      <Nav />
+      <div className="contentContainer">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/detail/:id' element={<Detail />} />
+          <Route path='/edit/:id' element={<Edit />} />
+          <Route path='/*' element={<Notfound />} />
+        </Routes>
+      </div>
     </>
   )
 }
