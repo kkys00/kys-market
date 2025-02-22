734c86e3db938709e4a147921a822899bcd095d7
Yeongseo Kim <1903020@ewhain.net>
2025-02-22 15:38:29 +0900

    feat: Header 컴포넌트 추가 및 레이아웃 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom'
 import './Header.css'
 
-const Header = () => {
+const Header = ({ rightChildFirst, rightChildSecond }) => {
     return (
-        <div>Header</div>
+        <div className="Header">
+            <div className="header_title">
+                <Link to="/">
+                    <h1>KYSMarket</h1>
+                </Link>
+            </div>
+            <div className="header_search">
+                <input placeholder="검색어를 입력하세요" />
+            </div>
+            <div className="header_rightChild">
+                <div>로그인</div>
+                <div>회원가입</div>
+            </div>
+        </div>
     )
 }
 
