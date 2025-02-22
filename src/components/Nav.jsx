0a30f1c6210cf4661fe0e5a8be94619c5c792340
Yeongseo Kim <1903020@ewhain.net>
2025-02-22 16:07:23 +0900

    feat: Nav 컴포넌트 추가 및 레이아웃 구현

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,16 @@
 import './Nav.css'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav className='Nav'>
+            <ul className="nav-list">
+                <li><Link to="/season">시즌</Link></li>
+                <li><Link to="/market">중고거래</Link></li>
+                <li><Link to="/groupbuy">공동구매</Link></li>
+                <li><Link to="/brand">동문브랜드</Link></li>
+            </ul>
+        </nav>
     )
 }
 
