291de39d8261681c3d778c342e772557f694bf5a
Yeongseo Kim <1903020@ewhain.net>
2025-02-23 15:51:10 +0900

    feat: Item 컴포넌트 추가 및 레이아웃 구현

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,17 @@
 import './Item.css'
 
-const Item = () => {
+const Item = ({ id, imgSrc, seller, product, price }) => {
     return (
-        <div>Item</div>
+        <div className="Item">
+            <div className="itemImgWrapper">
+                <img src={`/uploads/${imgSrc}`} alt="상품 이미지" />
+            </div>
+            <div className="itemInfoWrapper">
+                <div className="itemPrice">{price}원</div>
+                <div className="itemProduct">{product}</div>
+                <div className="itemSeller">판매자: {seller}</div>
+            </div>
+        </div>
     )
 }
 
