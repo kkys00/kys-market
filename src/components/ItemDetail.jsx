c65f4ed3c785a8fa57e2be17c6ae1d648e018168
Yeongseo Kim <1903020@ewhain.net>
2025-03-03 00:03:40 +0900

    feat: Detail 상세페이지를 위한 ItemDetail 컴포넌트 추가 및 구현

---
+++ src/components/ItemDetail.jsx
@@ -0,0 +1,57 @@
+import Button from './Button'
+import './ItemDetail.css'
+import { ShoppingCart, Bookmark } from 'lucide-react';
+
+const ItemDetail = ({ item }) => {
+    const {
+        title,
+        seller,
+        price,
+        description,
+        transactionMethod,
+        imgUrl,
+    } = item
+    const [mainImg, ...additionalImg] = imgUrl
+
+    return (
+        <div className='ItemDetail'>
+            <div className='imageWrapper'>
+                <div className='additionalImage'>
+                    {additionalImg.map(item => <img key={item} src={`/uploads/${item}`} />)}
+                </div>
+                <div className='mainImage'>
+                    <img src={`/uploads/${mainImg}`} alt="상품 이미지" />
+                </div>
+            </div>
+            <div className='infoWrapper'>
+                <div className='topInfo'>
+                    <div className='bar'></div>
+                    <div className='wordWrapper'>
+                        <h4>{title}</h4>
+                        <h4>{price}</h4>
+                    </div>
+                </div>
+                <div className='subInfo'>
+                    <div className='dataName'>판매자</div> <div className='bar'></div> <div className='data'>{seller}</div>
+                    <div className='dataName'>거래방법</div> <div className='bar'></div> <div className='data'>{transactionMethod.map((item) => <span key={item}>{item}</span>)}</div>
+                    <div className='dataName'>상품설명</div> <div className='bar'></div> <div className='data'>{description}</div>
+                </div>
+                <div className='buttons'>
+                    <Button text={
+                        <>
+                            <Bookmark size={18} color='white' /> 관심
+                        </>
+
+                    } type={"darkgreen"} />
+                    <Button text={
+                        <>
+                            <ShoppingCart size={18} color="white" /> 바로 구매
+                        </>
+                    } type={"darkgreen"} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ItemDetail
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,8 +1,32 @@
-import './Detail.css'
+import { useContext } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { ItemStateContext } from "../App"
+import ContentHeader from "../components/ContentHeader"
+import Button from "../components/Button"
+import ItemDetail from "../components/ItemDetail"
 
 const Detail = () => {
+    const nav = useNavigate()
+    const { id } = useParams()
+    const item = useContext(ItemStateContext)[id]
+
     return (
-        <div>Detail</div>
+        <div> {id} Detail
+            <ContentHeader
+                title={"중고거래"}
+                leftChild={
+                    <Button
+                        onClick={() => nav(-1)}
+                        text={"뒤로 가기"}
+                        type={"pinkred"} />
+                }
+                rightChild={
+                    <Button
+                        text={"리뷰 작성하기"}
+                        type={"mintgreen"} />
+                } />
+            <ItemDetail item={item} />
+        </div>
     )
 }
 
