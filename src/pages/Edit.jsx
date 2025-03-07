be57887c9a5458423dbc6b9f247f79d5d4645511
Yeongseo Kim <1903020@ewhain.net>
2025-03-07 20:12:06 +0900

    feat: onUpdate 구현 및 연결

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,8 +1,30 @@
-import './Edit.css'
+import { useParams } from "react-router-dom"
+import Editor from "../components/Editor"
+import { useContext } from "react"
+import { ItemDispatchContext, ItemStateContext } from "../App"
 
 const Edit = () => {
+    const { id } = useParams()
+    const item = useContext(ItemStateContext)[id]
+    const { onUpdate } = useContext(ItemDispatchContext)
+
+    const onSubmit = (input) => {
+        onUpdate(
+            id,
+            input.title,
+            input.seller,
+            input.price,
+            input.description,
+            input.transactionMethod,
+            input.imgUrl,
+            input.createdAt
+        )
+    }
+
     return (
-        <div>Edit</div>
+        <div>
+            <Editor type={'수정'} initData={item} onSubmit={onSubmit} />
+        </div>
     )
 }
 
